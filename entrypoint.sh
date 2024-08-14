176f74faed309d0fe5034cb3e93bd923c0e8caa1
Hubert Walczak <hubertwalczak8@gmail.com>
2024-08-14 08:37:04 +0200

    added stuf

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,5 +22,25 @@ if [ -z "$AWS_DEFAULT_REGION" ]; then
   exit 1
 fi
 
-ls -al
 
+
+mkdir -p ~/.aws
+touch ~/.aws/credentials
+
+echo "[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
+
+
+echo "Install yarn"
+npm install
+
+echo "Run npm build"
+npm run build
+
+echo "Copying to website folder"
+aws s3 sync ./build s3://${AWS_S3_BUCKET} --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
+
+echo "Cleaning up things"
+
+rm -rf ~/.aws
